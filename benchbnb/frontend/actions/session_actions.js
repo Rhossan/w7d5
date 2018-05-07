08c20710b8f8ef48c8a75ec00355def19fb88cfe
Rhossan <rhossan93@gmail.com>
2018-05-07 13:10:27 -0400

    actions, fixed signup

--- benchbnb/frontend/actions/session_actions.js
+++ benchbnb/frontend/actions/session_actions.js
@@ -4,12 +4,26 @@ export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
-export const receiveCurrentUser = currentUser => ({
-  type: RECEIVE_CURRENT_USER,
-  currentUser
-});
 
 export const signup = user => dispatch => (
   APIUtil.signup(user).then(user => (
-    dispatch(receiveCurrentUser(user))
+    dispatch({
+      type: RECEIVE_CURRENT_USER,
+      user
+    })
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then(user => (
+    dispatch({
+      type: RECEIVE_CURRENT_USER,
+      user
+    })
+  )
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then(user => (
+    dispatch(logoutCurrentUser())
+  ))
 );
